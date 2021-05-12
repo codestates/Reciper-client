45348278cf7c7ab0bb9814bbd192b81a3c007b92
Woogie-94 <keu1106@naver.com>
2021-05-12 11:34:11 +0900

    Add onChange event

--- src/components/Common/Input/index.tsx
+++ src/components/Common/Input/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { InputTag } from './styles';
 
 interface Props {
 	width: string;
 	height: string;
 	placeholderText: string;
+	onEvent: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input = (props: Props): JSX.Element => {
-	return <InputTag {...props} placeholder={props.placeholderText} />;
+	return <InputTag {...props} placeholder={props.placeholderText} onChange={props.onEvent} />;
 };
 
 Input.defaultProps = {
