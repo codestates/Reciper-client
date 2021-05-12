f85c3dae94cb7c500ccf4b97116ea28aaf9671eb
Woogie-94 <keu1106@naver.com>
2021-05-13 00:09:58 +0900

    Set styled-components css delete

--- src/components/Common/Select/styles.ts
+++ src/components/Common/Select/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
 export const SelectForm = styled.div<{ height: string; margin: string }>`
