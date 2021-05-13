f280e2574169a401206a65de6e49ce3c926771e0
Woogie-94 <keu1106@naver.com>
2021-05-14 02:47:27 +0900

    Add #40 common components StackTag css changed

--- src/components/Common/StackTag/styles.ts
+++ src/components/Common/StackTag/styles.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
+import { AiOutlineClose } from 'react-icons/ai';
 
-export const StackTagContainer = styled.span`
-	height: 20px;
-	padding: 3.5px 10px;
+export const StackTagContainer = styled.span<{ type: string }>`
+	position: relative;
+	padding: ${({ type }) => (type === 'delete' ? `6px 25px 6px 10px` : '6px 10px')};
 	margin-right: 5px;
 	font-family: 'NanumSquareB';
-	font-size: 12px;
+	font-size: 14px;
 	color: ${({ theme }) => theme.color.pointColor};
 	background-color: #eeebff;
+	border-radius: 3px;
+`;
+
+export const StackTagCloseIcon = styled(AiOutlineClose)`
+	cursor: pointer;
+	position: absolute;
+	top: 8px;
+	right: 7px;
+	font-size: 12px;
 `;
