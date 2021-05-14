9ec034a9de0cbe762341dcb61a63c87c3bd8f38a
Woogie-94 <keu1106@naver.com>
2021-05-14 12:03:58 +0900

    Add recruit types & stackData type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,6 @@
+////////////////////////////////////////////////////////
+///////////            Login             //////////////
+///////////////////////////////////////////////////////
 export interface loginRequestType {
 	authorizationCode: string | null;
 	email: string | null;
@@ -8,4 +11,46 @@ export interface loginResponseDataType {
 	accessToken: string;
 	email: string;
 	loginType: string;
+}
+
+////////////////////////////////////////////////////////
+///////////            Recruit           //////////////
+///////////////////////////////////////////////////////
+export interface recruitMembersDataType {
+	position: string;
+	career: string;
+	personnel: string;
+	deadline: string;
+}
+
+export interface recruitCreateTopDataType {
+	simple_desc: string;
+	recruit_members: recruitMembersDataType[];
+	require_stack: string[];
+	service_step: string;
+	period: string;
+}
+
+export interface recruitCreateBottomDataType {
+	detail_title: string;
+	detail_desc: string;
+}
+
+export interface recruitCreateDataType {
+	name: string;
+	simple_desc: string;
+	recruit_members: recruitMembersDataType[];
+	require_stack: string[];
+	service_step: string;
+	period: string;
+	detail_title: string;
+	detail_desc: string;
+}
+
+////////////////////////////////////////////////////////
+///////////            Recruit           //////////////
+///////////////////////////////////////////////////////
+export interface stackData {
+	id: number;
+	name: string;
 }
