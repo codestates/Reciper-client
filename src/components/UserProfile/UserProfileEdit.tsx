4b2b5538c8728a5def8d3940d5a76c9bbae21d69
useonglee <useong0830@naver.com>
2021-05-14 22:52:10 +0900

    Add #37 profile image upload feature

--- src/components/UserProfile/UserProfileEdit.tsx
+++ src/components/UserProfile/UserProfileEdit.tsx
@@ -1,5 +1,5 @@
-import React, { useState, KeyboardEvent } from 'react';
-import { getProfileInfoSelector } from '../../reducer/profile';
+import React, { useState, KeyboardEvent, ChangeEvent, useEffect, useRef } from 'react';
+import { getProfileInfo, getProfileInfoSelector } from '../../reducer/profile';
 import { getProfileEdit } from '../../reducer/profileEdit';
 
 import { IoMdClose } from 'react-icons/io';
@@ -29,40 +29,49 @@ import {
 	CurrentStack,
 	StackMaximum,
 	ToggleMessage,
+	Profile_UserImage,
 } from './styles';
 
 import { profileEditType } from '../../types/types';
+import axios from 'axios';
 
 const UserProfileEdit = (): JSX.Element => {
 	const [stackName, onChangeStackName, setStackName] = useInput<string>('');
 	const [stackBucket, setStackBucket] = useState<string[]>([]);
 	const [isToggled, setIsToggled] = useState<boolean>(false);
+	const [image, setImage] = useState<string>('');
 	const history = useHistory();
 	const dispatch = useDispatch();
 	const profileInfo = useSelector(getProfileInfoSelector);
+	const imageInput = useRef<HTMLInputElement>(null);
 
 	const [name, onChangeName] = useInput<string>(profileInfo.name);
 	const [mobile, onChangeMobile] = useInput<string>(profileInfo.mobile);
-	const [about_me, onChangeAbout_me] = useInput<string>(profileInfo.about_me);
-	const [git_id, onChangeGit_id] = useInput<string>(profileInfo.git_id);
+	const [aboutMe, onChangeAbout_me] = useInput<string>(profileInfo.aboutMe);
+	const [gitId, onChangeGit_id] = useInput<string>(profileInfo.gitId);
 	const [office, onChangeoffice] = useInput<string>(profileInfo.career.office);
 	const [job, onChangeJob] = useInput<string>(profileInfo.career.job);
 	const [period, onChangePeriod] = useInput<string>(profileInfo.career.period);
 
+	useEffect(() => {
+		dispatch(getProfileInfo());
+	}, []);
+
 	const data: profileEditType = {
 		name: name,
 		mobile: mobile,
-		git_id: git_id,
+		gitId: gitId,
 		career: {
 			office: office,
 			job: job,
 			period: period,
 		},
 		isOpen: false,
-		about_me: about_me,
-		profile_image: '',
-		stacks: [],
+		aboutMe: aboutMe,
+		profileImage: image,
+		stacks: [...stackBucket],
 	};
+	console.log('data', data);
 
 	const onAddStack = (): void => {
 		if (stackName.trim() === '') {
@@ -88,8 +97,32 @@ const UserProfileEdit = (): JSX.Element => {
 		history.push(`/profile/${profileInfo.id}`);
 	};
 
-	const onImageUpload = (): void => {
-		// TODO: 이미지 업로드 하기
+	// TODO: 이미지 업로드 연습하는 부분
+	const localStorage_loginInfo = window.localStorage.getItem('loginInfo') as string;
+	const loginInfo = JSON.parse(localStorage_loginInfo);
+	const userAccessToken = loginInfo.accessToken;
+	const userLoginType = loginInfo.loginType;
+
+	const onChnageImage = async (e: ChangeEvent<HTMLInputElement>) => {
+		const formData = new FormData();
+		if (e.target.files) {
+			formData.append('file', e.target.files[0]);
+			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/profile`, formData, {
+				headers: {
+					authorization: `Bearer ${userAccessToken}`,
+					loginType: userLoginType,
+				},
+			});
+			const imageData = response.data.profileImage as string;
+			setImage(imageData);
+			console.log('이미지 확인', imageData);
+		}
+	};
+
+	const onUploadImage = (): void => {
+		if (imageInput.current) {
+			imageInput.current.click();
+		}
 	};
 
 	const onResetImage = (): void => {
@@ -104,13 +137,34 @@ const UserProfileEdit = (): JSX.Element => {
 
 			{/* TODO: 유저 개인 정보 */}
 			<Profile_UserCard>
-				<ProfileEdit_Img onClick={onImageUpload}>
-					<div>
-						<div>{profileInfo.name.slice(0, 1)}</div>
-					</div>
+				<ProfileEdit_Img>
+					{/* TODO: 이미지 요청 테스트중 */}
+					<form encType="multipart/form-data">
+						<input
+							type="file"
+							accept="image/jpg,image/png,/image/jpeg"
+							name="file"
+							hidden
+							onChange={onChnageImage}
+							ref={imageInput}
+						/>
+					</form>
+					{profileInfo.profileImage !== '' ? (
+						<div onClick={onUploadImage}>
+							<Profile_UserImage src={`${process.env.REACT_APP_SERVER_URL}/images/${image}`} alt="" />
+						</div>
+					) : (
+						<div style={{ backgroundColor: `${profileInfo.profileColor}` }} onClick={onUploadImage}>
+							{profileInfo.name ? (
+								<div style={{ margin: '45px', fontSize: '110px' }}>{profileInfo.name.slice(0, 1)}</div>
+							) : (
+								<div style={{ margin: '20px', fontSize: '130px' }}>{profileInfo.email.slice(0, 1)}</div>
+							)}
+						</div>
+					)}
+
 					<span onClick={onResetImage}>삭제</span>
 				</ProfileEdit_Img>
-
 				<Profile_UserInfoCard>
 					<div>
 						<Profile_SubTitle>이름</Profile_SubTitle>
@@ -136,7 +190,7 @@ const UserProfileEdit = (): JSX.Element => {
 							<Input
 								width="long"
 								placeholderText="최대 20자 내로 적어주세요"
-								initValue={about_me}
+								initValue={aboutMe}
 								changeEvent={onChangeAbout_me}
 							/>
 						</Profile_UserInfo>
@@ -150,7 +204,7 @@ const UserProfileEdit = (): JSX.Element => {
 					<div>
 						<Profile_SubTitle>Github 아이디</Profile_SubTitle>
 						<Profile_UserInfo>
-							<Input placeholderText="아이디를 입력하세요" initValue={git_id} changeEvent={onChangeGit_id} />
+							<Input placeholderText="아이디를 입력하세요" initValue={gitId} changeEvent={onChangeGit_id} />
 						</Profile_UserInfo>
 					</div>
 					<div>
