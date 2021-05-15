b7445e6d2409dd091673e45f4d1c702edea1b837
Woogie-94 <keu1106@naver.com>
2021-05-16 02:12:14 +0900

    Add #65 data output after data request

--- src/components/RecruitDetail/DetailContainer/index.tsx
+++ src/components/RecruitDetail/DetailContainer/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
 
 import DetailComment from '../DetailComment';
 import DetailContent from '../DetailContent';
@@ -9,11 +11,63 @@ import { FullDiv } from './styles';
 import { Container } from './styles';
 
 const DetailContainer = (): JSX.Element => {
+	const { id: params } = useParams<{ id: string }>();
+
+	const [topData, setTopData] = useState({
+		name: '',
+		view: 0,
+		commentCount: 0,
+		simpleDesc: '',
+	});
+
+	const [contentData, setContentData] = useState({
+		detailTitle: '',
+		recruitImage: '',
+		detailDesc: '',
+		recruitMembers: [],
+		requireStack: [],
+		serviceStep: '',
+		period: '',
+	});
+
+	useEffect(() => {
+		const localStorage_loginInfo = window.localStorage.getItem('loginInfo') as string;
+		const { accessToken, loginType } = JSON.parse(localStorage_loginInfo);
+
+		axios
+			.get(`${process.env.REACT_APP_SERVER_URL}/recruitBoard/${params}`, {
+				headers: { authorization: `Bearer ${accessToken}`, loginType },
+			})
+			.then(data => {
+				console.log(data.data);
+				const {
+					name,
+					commentsList,
+					view,
+					simpleDesc,
+					detailTitle,
+					recruitImage,
+					detailDesc,
+					recruitMembers,
+					requireStack,
+					serviceStep,
+					period,
+				} = data.data;
+
+				setTopData({ name, view, commentCount: commentsList.length, simpleDesc });
+				setContentData({ detailTitle, recruitImage, detailDesc, recruitMembers, requireStack, serviceStep, period });
+			});
+	}, []);
+
+	/*
+		top = name, commantList.length, view, simpleDesc
+		content = detailTitle, recruitImage, detailDesc, recruitMembers, requireStack, serviceStep, period
+	*/
 	return (
 		<FullDiv>
 			<Container>
-				<DetailTop />
-				<DetailContent />
+				<DetailTop {...topData} />
+				<DetailContent {...contentData} />
 				<DetailWriter />
 				<DetailComment />
 			</Container>
--- src/components/RecruitDetail/DetailContent/index.tsx
+++ src/components/RecruitDetail/DetailContent/index.tsx
@@ -16,42 +16,58 @@ import {
 	DetailDeadLineText,
 } from './styles';
 
-const DetailContent = (): JSX.Element => {
-	const recruitEx = [
-		// 디자인 확인용 데이터입니다. 삭제 바람
-		{ position: '프론트엔드', career: '경력무관', personnel: '2', deadline: '2021-07-01' },
-		{ position: '백엔드', career: '2년 이상', personnel: '1', deadline: '2021-09-01' },
-	];
-	const stackEx = ['React', 'Typescript']; // 디자인 확인용 데이터입니다. 삭제 바람
+interface RecruitMember {
+	position: string;
+	career: string;
+	personner: string;
+	deadline: string;
+}
 
+interface RecruitDetailConentData {
+	detailTitle: string;
+	recruitImage: string;
+	detailDesc: string;
+	recruitMembers: RecruitMember[];
+	requireStack: string[];
+	serviceStep: string;
+	period: string;
+}
+
+const DetailContent = ({
+	detailTitle,
+	recruitImage,
+	detailDesc,
+	recruitMembers,
+	requireStack,
+	serviceStep,
+	period,
+}: RecruitDetailConentData): JSX.Element => {
 	return (
 		<DetailContentContainer>
-			<DetailSubTitle>Reciper 같이 만드실 분 구합니다.</DetailSubTitle>
-			<DetailImg />
-			<DetailDescription>
-				프로젝트 협업 커뮤니티 겸 협업을 위한 툴을 제작하려 합니다. <br /> 기본적이 아이디어 구상은 끝 마쳤으나 변경이
-				필요한 부분이나 부족한 부분을 같이 채워 나갈 개발자를 구하고 있습니다. <br /> 현재 프론트엔드 개발자 한 분과
-				백엔드 개발자 한 분이 있고 추가적으로 프론트엔드 백엔드 개발자 두 분을 더 모시려고 합니다.
-			</DetailDescription>
+			<DetailSubTitle>{detailTitle}</DetailSubTitle>
+			<DetailImg>
+				<img src={`${process.env.REACT_APP_SERVER_URL}/images/${recruitImage}`} />
+			</DetailImg>
+			<DetailDescription>{detailDesc}</DetailDescription>
 
 			<DetailRecruiteTitle>모집 인원</DetailRecruiteTitle>
-			{recruitEx.map((list, index) => (
+			{recruitMembers.map((list, index) => (
 				<DetailRecruiteList key={index}>
 					{`- ${list.position}`}
-					<span>{`${list.career}/${list.personnel}명/~${list.deadline}까지`}</span>
+					<span>{`${list.career}/${list.personner}명/~${list.deadline}까지`}</span>
 				</DetailRecruiteList>
 			))}
 
 			<DetailStackTitle>기술 스택</DetailStackTitle>
-			{stackEx.map((stack, index) => (
+			{requireStack.map((stack, index) => (
 				<StackTag key={index}>{stack}</StackTag>
 			))}
 
 			<DetailStageTitle>개발 단계</DetailStageTitle>
-			<DetailStageText>초기 개발 단계</DetailStageText>
+			<DetailStageText>{serviceStep}</DetailStageText>
 
 			<DetailDeadLineTitle>목표 기한</DetailDeadLineTitle>
-			<DetailDeadLineText>~2021-06-07</DetailDeadLineText>
+			<DetailDeadLineText>{`~${period}`}</DetailDeadLineText>
 		</DetailContentContainer>
 	);
 };
