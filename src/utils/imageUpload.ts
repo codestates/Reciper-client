7dcf779ef3e8f1c107acc155685a96564559c4bc
Woogie-94 <keu1106@naver.com>
2021-05-16 11:23:30 +0900

    Set  endPoint

--- src/utils/imageUpload.ts
+++ src/utils/imageUpload.ts
@@ -6,20 +6,21 @@ const { accessToken, loginType } = JSON.parse(localStorage_loginInfo);
 
 export const changeImage = async (
 	e: ChangeEvent<HTMLInputElement>,
+	endPoint: string,
 	setState: React.Dispatch<React.SetStateAction<string>>,
 ): Promise<void> => {
 	const formData = new FormData();
 
 	if (e.target.files) {
 		formData.append('file', e.target.files[0]);
-		const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/profile`, formData, {
+		const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}${endPoint}`, formData, {
 			headers: {
 				authorization: `Bearer ${accessToken}`,
 				loginType,
 			},
 		});
 
-		const imageData = response.data.profileImage;
+		const imageData = response.data.uplodaImage;
 		setState(imageData);
 	}
 };
