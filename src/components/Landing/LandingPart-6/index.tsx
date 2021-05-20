db4a7f904a59012fa64bd856fe7cff6023ad9c9a
useonglee <useong0830@naver.com>
2021-05-21 00:26:53 +0900

    Fix #124 Changed padding value

--- src/components/Landing/LandingPart-6/index.tsx
+++ src/components/Landing/LandingPart-6/index.tsx
@@ -1,10 +1,35 @@
-import React, { useEffect } from 'react';
-import { LandingSixthBottom, LandingSixthContainer, LandingSixthTop, SvgWrapper } from './styles';
+import React, { useEffect, useState } from 'react';
+
+import {
+	LandingSixthBottom,
+	LandingSixthContainer,
+	LandingSixthTop,
+	PersonalAvatar,
+	PersonalCard,
+	PersonalCode,
+	SvgWrapper,
+	TopContentsWrapper,
+	TopMessage,
+	TopPersonalCardWrapper,
+} from './styles';
 
 const LandingSixth = (): JSX.Element => {
+	const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+	const onScroll = () => {
+		setScrollPosition(window.pageYOffset);
+		// console.log('마우스 위치', window.pageYOffset);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', onScroll);
+		// 컴포넌트가 언마운트 되기 직전에 이벤트를 끝낸다. 메모리 누수 방지
+		return () => window.removeEventListener('scroll', onScroll);
+	}, []);
+
 	useEffect(() => {
 		const svg: HTMLCollectionOf<Element> = document.getElementsByClassName('rainbowLine');
-		console.log('svg 길이', length);
+		// console.log('svg 길이', length);
 
 		Array.from(svg).forEach(line => {
 			if (line instanceof SVGPathElement) {
@@ -41,10 +66,74 @@ const LandingSixth = (): JSX.Element => {
 	return (
 		<LandingSixthContainer>
 			<LandingSixthTop>
-				<div>test</div>
+				<TopContentsWrapper>
+					<TopMessage>개발의 문화</TopMessage>
+					<TopPersonalCardWrapper>
+						<PersonalCard style={{ borderBottom: `${scrollPosition > 7200 ? 'none' : '1px solid #d6d6d8'}` }}>
+							<PersonalAvatar></PersonalAvatar>
+							<div
+								style={{
+									opacity: `${(scrollPosition - 7200) / 50}`,
+									transition: '0.3s',
+									backgroundColor: '#3d88b0',
+								}}
+							>
+								<PersonalCode />
+							</div>
+						</PersonalCard>
+						<PersonalCard style={{ borderBottom: `${scrollPosition > 7200 ? 'none' : '1px solid #d6d6d8'}` }}>
+							<PersonalAvatar></PersonalAvatar>
+							<div
+								style={{
+									opacity: `${(scrollPosition - 7200) / 50}`,
+									transition: '0.3s',
+									backgroundColor: '#a17398',
+								}}
+							>
+								<PersonalCode />
+							</div>
+						</PersonalCard>
+						<PersonalCard style={{ borderBottom: `${scrollPosition > 7200 ? 'none' : '1px solid #d6d6d8'}` }}>
+							<PersonalAvatar></PersonalAvatar>
+							<div
+								style={{
+									opacity: `${(scrollPosition - 7200) / 50}`,
+									transition: '0.3s',
+									backgroundColor: '#f36652',
+								}}
+							>
+								<PersonalCode />
+							</div>
+						</PersonalCard>
+						<PersonalCard style={{ borderBottom: `${scrollPosition > 7200 ? 'none' : '1px solid #d6d6d8'}` }}>
+							<PersonalAvatar></PersonalAvatar>
+							<div
+								style={{
+									opacity: `${(scrollPosition - 7200) / 50}`,
+									transition: '0.3s',
+									backgroundColor: '#f79649',
+								}}
+							>
+								<PersonalCode />
+							</div>
+						</PersonalCard>
+						<PersonalCard style={{ borderBottom: `${scrollPosition > 7200 ? 'none' : '1px solid #d6d6d8'}` }}>
+							<PersonalAvatar></PersonalAvatar>
+							<div
+								style={{
+									opacity: `${(scrollPosition - 7200) / 50}`,
+									transition: '0.5s',
+									backgroundColor: '#ffc233',
+								}}
+							>
+								<PersonalCode />
+							</div>
+						</PersonalCard>
+					</TopPersonalCardWrapper>
+				</TopContentsWrapper>
 				<div>
 					<SvgWrapper
-						viewBox="0 0 1300 1300"
+						viewBox="0 550 1300 1300"
 						xmlns="http://www.w3.org/2000/svg"
 						fill="evenodd"
 						clipRule="evenodd"
@@ -54,35 +143,35 @@ const LandingSixth = (): JSX.Element => {
 					>
 						<path
 							className="rainbowLine"
-							d="M500 550v50c0 0-0 0-0 100H1055c0 0-0 0-0 50v50c0 0-0 0-0 100H400c0 0-0 0-0 100v100 0-0 0-0 80H700c0 0-0 0-0 100v390"
+							d="M700 550v50c0 0-0 0-0 100H1055c0 0-0 0-0 50v50c0 0-0 0-0 100H400c0 0-0 0-0 100v100 0-0 0-0 80H700c0 0-0 0-0 100v380"
 							fill="none"
 							stroke="#ffc233"
 							strokeWidth="40"
 						/>
 						<path
 							className="rainbowLine"
-							d="M420 550v150 c0 0-0 00-00 100H575c-0 0-0 0-0 20v150c0 0-0 0-0 100H750c-0 0-0 0-0 50v160 0-0 0-0 100v390"
+							d="M570 550v150 c0 0-0 00-00 100H275c-0 0-0 0-0 20v150c0 0-0 0-0 100H750c-0 0-0 0-0 50v160 0-0 0-0 100v280"
 							fill="none"
 							stroke="#f79649"
 							strokeWidth="40"
 						/>
 						<path
 							className="rainbowLine"
-							d="M340 550v210 c0 0-0 00-00 100H975c-00 0-0 0-0 10v200c0 0-0 0-0 100H650c-0 0-0 0-0 100v390"
+							d="M440 550v210 c0 0-0 00-00 100H975c-00 0-0 0-0 10v200c0 0-0 0-0 100H650c-0 0-0 0-0 100v390"
 							fill="none"
 							stroke="#f36652"
 							strokeWidth="40"
 						/>
 						<path
 							className="rainbowLine"
-							d="M260 550v280 c0 0-0 00-00 100H1175c-0 0-0 0-0 10v200c0 0-0 0-0 100H600c-0 0-0 0-0 100v390"
+							d="M310 550v280 c0 0-0 00-00 100H1175c-0 0-0 0-0 10v200c0 0-0 0-0 100H600c-0 0-0 0-0 100v320"
 							fill="none"
 							stroke="#a17398"
 							strokeWidth="40"
 						/>
 						<path
 							className="rainbowLine"
-							d="M180 550v350 c0 0-0 00-00 100H1075c-00 0-0 0-0 10v200c0 0-0 0-0 100H550c-0 0-0 0-0 100v390"
+							d="M180 550v350 c0 0-0 00-00 100H875c-00 0-0 0-0 10v200c0 0-0 0-0 100H550c-0 0-0 0-0 100v250"
 							fill="none"
 							stroke="#3d88b0"
 							strokeWidth="40"
@@ -91,7 +180,7 @@ const LandingSixth = (): JSX.Element => {
 				</div>
 			</LandingSixthTop>
 			<LandingSixthBottom>
-				<div>안녕</div>
+				<div style={{ opacity: `${scrollPosition > 8850 ? `1` : `0.3`}`, transition: '0.3s' }}>트렌드의 시작</div>
 			</LandingSixthBottom>
 		</LandingSixthContainer>
 	);
--- src/components/Landing/LandingPart-6/styles.ts
+++ src/components/Landing/LandingPart-6/styles.ts
@@ -1,27 +1,85 @@
 import styled from 'styled-components';
+import { HiCode } from 'react-icons/hi';
 
 export const LandingSixthContainer = styled.div`
+	position: relative;
 	width: 100%;
+	height: 2900px;
+	padding: 350px 0;
 `;
 
 export const LandingSixthTop = styled.div`
 	${({ theme }) => theme.align.flexCenter};
 	flex-direction: column;
+	max-height: 1800px;
 `;
+
+// ---------------- TODO: Card ----------------
+
+export const TopContentsWrapper = styled.div`
+	${({ theme }) => theme.align.flexHorizontal};
+	align-items: flex-start;
+	flex-direction: column;
+	margin-right: 580px;
+`;
+
+export const TopMessage = styled.div`
+	font-family: NanumSquareEB;
+	font-size: 160px;
+	color: ${({ theme }) => theme.color.lineColor};
+`;
+
+export const TopPersonalCardWrapper = styled.div`
+	${({ theme }) => theme.align.flexVertical};
+`;
+
+export const PersonalCard = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 160px;
+	height: 200px;
+	margin: 0 10px;
+	border: 0.3px solid ${({ theme }) => theme.color.lineColor};
+
+	& > div {
+		${({ theme }) => theme.align.flexCenter};
+	}
+`;
+
+export const PersonalAvatar = styled.div`
+	height: 60%;
+	border-bottom: 1px solid ${({ theme }) => theme.color.lineColor};
+`;
+
+export const PersonalCode = styled(HiCode)`
+	height: 80px;
+	font-size: 72px;
+	color: #333333;
+`;
+
+// ---------------- TODO: SVG ----------------
 
 export const SvgWrapper = styled.svg`
 	position: relative;
 	z-index: 5;
 	width: 1800px;
-	height: 2300px;
+	height: 1800px;
 `;
+
+// ---------------- TODO: Bottom ----------------
 
 export const LandingSixthBottom = styled.div`
 	${({ theme }) => theme.align.flexCenter};
 	flex-direction: column;
 	position: relative;
-	bottom: 500px;
+	bottom: 400px;
 	width: 100%;
 	min-height: 100vh;
 	background-color: #eeeff2;
+
+	& > div {
+		font-family: NanumSquareEB;
+		font-size: 160px;
+		color: #444444;
+	}
 `;
