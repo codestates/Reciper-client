1ac615ec906a19ab7602ac04ad39b509249a9c1a
Woogie-94 <keu1106@naver.com>
2021-05-24 19:31:32 +0900

    Add #183 kanban page drag and drop feature

--- src/components/Kanban/KanbanContainer/index.tsx
+++ src/components/Kanban/KanbanContainer/index.tsx
@@ -1,9 +1,15 @@
 import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import useInput from '../../../hooks/useInput';
-import { addTaskBox, kanbanDataSelector } from '../../../reducer/kanban';
-import { kanbanDataType } from '../../../types/types';
+import { DragDropContext, DragStart, Droppable, DropResult } from 'react-beautiful-dnd';
+
 import TaskBox from '../TaskBox';
+
+import useInput from '../../../hooks/useInput';
+
+import { kanbanDataType } from '../../../types/types';
+
+import { addTaskBox, kanbanDataSelector, reorderTaskBox, reorerTaskItem } from '../../../reducer/kanban';
+
 import { AddTaskBoxBtn, AddTaskBoxInput, Container, TaskBoxWrap } from './styles';
 
 const KanbanConianer = (): JSX.Element => {
@@ -23,24 +29,64 @@ const KanbanConianer = (): JSX.Element => {
 		setShowAddTask(false);
 	}, [title]);
 
+	const onDragStart = (initial: DragStart): void => {
+		if (initial.type === 'TaskBox') {
+			const targetBox = document.querySelector(`.${initial.draggableId}`);
+			targetBox?.classList.add('dragging');
+		}
+	};
+
+	const onDragEnd = (result: DropResult): void => {
+		const { type, source, destination } = result;
+		console.log(result);
+		if (!destination) {
+			return;
+		}
+
+		if (type === 'TaskBox') {
+			const currentIndex = source.index;
+			const targetIndex = destination.index;
+			const targetBox = document.querySelector(`.${result.draggableId}`);
+			targetBox?.classList.remove('dragging');
+
+			dispatch(reorderTaskBox({ currentIndex, targetIndex }));
+		}
+
+		if (type === 'TaskItem') {
+			const currentIndex = source.index;
+			const targetIndex = destination.index;
+			const currentListIndex = Number(source.droppableId.split('-')[1]);
+			const targetListIndex = Number(destination.droppableId.split('-')[1]);
+			console.log(currentIndex, targetIndex, currentListIndex, targetListIndex);
+			dispatch(reorerTaskItem({ currentIndex, targetIndex, currentListIndex, targetListIndex }));
+		}
+	};
+
 	return (
 		<Container>
-			<TaskBoxWrap>
-				{taskBox.map((taskBox, index) => (
-					<TaskBox key={index} taskBoxData={taskBox} index={index} />
-				))}
-				{showAddTaskForm ? (
-					<AddTaskBoxInput
-						placeholder="+ 테스크 박스를 추가하세요"
-						value={title}
-						onChange={onChangeTitle}
-						onKeyPress={e => e.key === 'Enter' && onAddTaskBox()}
-						onBlur={onAddTaskBox}
-					/>
-				) : (
-					<AddTaskBoxBtn onClick={() => setShowAddTask(true)}>+ 테스크 박스를 추가하세요</AddTaskBoxBtn>
-				)}
-			</TaskBoxWrap>
+			<DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+				<Droppable droppableId="TaskBoxArea" type="TaskBox" direction="horizontal">
+					{provided => (
+						<TaskBoxWrap ref={provided.innerRef}>
+							{taskBox.map((taskBox, index) => (
+								<TaskBox key={`TaskBox-${index}`} taskBoxData={taskBox} index={index} />
+							))}
+							{provided.placeholder}
+							{showAddTaskForm ? (
+								<AddTaskBoxInput
+									placeholder="+ 테스크 박스를 추가하세요"
+									value={title}
+									onChange={onChangeTitle}
+									onKeyPress={e => e.key === 'Enter' && onAddTaskBox()}
+									onBlur={onAddTaskBox}
+								/>
+							) : (
+								<AddTaskBoxBtn onClick={() => setShowAddTask(true)}>+ 테스크 박스를 추가하세요</AddTaskBoxBtn>
+							)}
+						</TaskBoxWrap>
+					)}
+				</Droppable>
+			</DragDropContext>
 		</Container>
 	);
 };
