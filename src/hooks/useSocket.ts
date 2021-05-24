1e99de83a97908e9c8646052dd525b8905d3ca64
useonglee <useong0830@naver.com>
2021-05-24 10:06:15 +0900

    Add #170 useSocket hook

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,47 @@
+import React, { useCallback } from 'react';
+import io from 'socket.io-client';
+
+const sockets: { [key: string]: any } = {};
+
+const useSocket = (projectUrl?: string): [any | undefined, () => void] => {
+	const disconnect = useCallback(() => {
+		if (projectUrl && sockets[projectUrl]) {
+			sockets[projectUrl].disconnect();
+			delete sockets[projectUrl];
+		}
+	}, [projectUrl]);
+
+	if (!projectUrl) {
+		return [undefined, disconnect];
+	}
+
+	// TODO: 연결이 안되어 있다면 요청하고, 되어있으면 한번만 연결할 수 있도록 해준다.
+	if (!sockets[projectUrl]) {
+		sockets[projectUrl] = io(`${process.env.REACT_APP_SERVER_URL}/project`, {
+			transports: ['websocket'],
+			query: {
+				projectURL: projectUrl,
+			},
+		});
+	}
+
+	// sockets[projectUrl].emit('totalMessageGet', (data: any) => {
+	// 	console.log('message', data);
+	// });
+
+	// sockets[projectUrl].emit('message', (data: any) => {
+	// 	console.log('message', data);
+	// });
+
+	// sockets[projectUrl].on('totalMessageGet', (data: any) => {
+	// 	console.log('totalMessageGet', data);
+	// });
+
+	// sockets[projectUrl].on('message', (data: any) => {
+	// 	console.log(data);
+	// });
+
+	return [sockets[projectUrl], disconnect];
+};
+
+export default useSocket;
