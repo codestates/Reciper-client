ea84963e4827020c370fca6d05a7c8a582a913ff
useonglee <useong0830@naver.com>
2021-05-24 10:06:58 +0900

    Add #171 chat textarea

---
+++ src/components/Chat/Textarea/index.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, KeyboardEvent, FormEvent, ChangeEvent, useRef } from 'react';
+
+import { Mention } from 'react-mentions';
+
+import { ChatArea, ChatForm, MentionsTextarea, SendChatBox, SendChatButton } from './styles';
+
+interface Props {
+	onSubmitForm: (e: FormEvent) => void;
+	onChangeChat: (e: ChangeEvent<HTMLInputElement>) => void;
+	chat?: string;
+	placeholder: string;
+	data?: string[];
+}
+
+const Textarea = ({ onSubmitForm, onChangeChat, chat, placeholder, data }: Props): JSX.Element => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const onKeydownChat = useCallback(
+		(e: KeyboardEvent<HTMLInputElement>): void => {
+			if (e.key === 'Enter') {
+				e.preventDefault();
+				onSubmitForm(e);
+			}
+		},
+		[onSubmitForm],
+	);
+
+	return (
+		<ChatArea>
+			<ChatForm className={!chat?.trim() ? 'off' : 'onValue'} onSubmit={onSubmitForm}>
+				<MentionsTextarea
+					value={chat}
+					onChange={onChangeChat}
+					onKeyPress={onKeydownChat}
+					placeholder={placeholder}
+					ref={inputRef}
+				></MentionsTextarea>
+				<SendChatBox className={!chat?.trim() ? 'off' : 'onValue'} type="submit">
+					<SendChatButton />
+				</SendChatBox>
+			</ChatForm>
+		</ChatArea>
+	);
+};
+
+export default Textarea;
