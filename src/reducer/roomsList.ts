1b325f3dbfe72d74037b58dbebb47c972550e7ef
useonglee <useong0830@naver.com>
2021-05-26 02:15:03 +0900

    Add #201 add chat room store

--- src/reducer/roomsList.ts
+++ src/reducer/roomsList.ts
@@ -3,11 +3,23 @@ import { axiosRequest } from '../utils/axios';
 
 import { RootStateOrAny } from 'react-redux';
 
-import { RoomsListDataType } from '../types/types';
+import { addRoomDataType, RoomNameType, RoomsListDataType } from '../types/types';
 
 // TODO: Thunk 실행
-export const getRoomsListInfo = createAsyncThunk('chatRoomInfo', (projectURL: string): Promise<void> => {
-	return axiosRequest('get', `/workspace/${projectURL}/chat`);
+// 조회
+export const getRoomsListInfo = createAsyncThunk('chatRoomInfo', (roomValue: addRoomDataType): Promise<void> => {
+	return axiosRequest('get', `/workspace/${roomValue.currentURL}/${roomValue.currentRoom}`);
+});
+// 생성
+export const addRoom = createAsyncThunk('addRoom', (roomValue: addRoomDataType): Promise<RoomNameType | void> => {
+	return axiosRequest('post', `/workspace/${roomValue.currentURL}/${roomValue.currentRoom}`, roomValue.roomName);
+});
+// 삭제
+export const deleteRoom = createAsyncThunk('deleteRoom', (roomValue: addRoomDataType): Promise<RoomNameType | void> => {
+	return axiosRequest(
+		'delete',
+		`/workspace/${roomValue.currentURL}/${roomValue.currentRoom}/${roomValue.roomName?.name}`,
+	);
 });
 
 // TODO: 초기 상태
@@ -22,6 +34,8 @@ export const roomsListInfoSlice = createSlice({
 	reducers: {},
 	extraReducers: {
 		[getRoomsListInfo.fulfilled.type]: (state, { payload }: PayloadAction<RoomsListDataType>) => payload,
+		[addRoom.fulfilled.type]: (state, { payload }: PayloadAction<RoomsListDataType>) => payload,
+		[deleteRoom.fulfilled.type]: (state, { payload }: PayloadAction<RoomsListDataType>) => payload,
 	},
 });
 
