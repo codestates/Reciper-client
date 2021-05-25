a772747f28a11427ded9acebd0ac261efbe7de6e
useonglee <useong0830@naver.com>
2021-05-25 10:20:33 +0900

    Make #196 Chat room division feature

--- src/components/Chat/ChatContainer/index.tsx
+++ src/components/Chat/ChatContainer/index.tsx
@@ -4,67 +4,70 @@ import { getProfileInfoSelector } from '../../../reducer/profile';
 import ChatZone from '../ChatZone';
 import Textarea from '../Textarea';
 import useSocket from '../../../hooks/useSocket';
-import { Scrollbars } from 'react-custom-scrollbars';
 
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-
-import { ChatDataType } from '../../../types/types';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { OnChangeHandlerFunc } from 'react-mentions';
 
 const WorkSpaceChat = (): JSX.Element => {
-	const listData = ['공지사항'];
+	const listData = ['공지사항', '자료'];
 	const profileInfo = useSelector(getProfileInfoSelector);
 	const [chat, setChat] = useState<string>('');
-	const { projectUrl, part } = useParams<{ projectUrl: string; part: string }>();
+	const { projectUrl, part: room } = useParams<{ projectUrl: string; part: string }>();
 	const [socket] = useSocket(projectUrl);
+	console.log(room);
 
 	// TODO: 채팅기록을 담아줄 바구니
-	const [chatBucket, setChatBucket] = useState<ChatDataType[]>([]);
+	const [chatBucket, setChatBucket] = useState<any[]>([]);
 
 	// TODO: 해당하는 채팅 Room과 연결 시도
 	useEffect(() => {
-		socket.emit('joinRoom', part);
-	}, [part]);
+		socket?.emit('joinRoom', room);
+	}, []);
 
 	// TODO: 이전까지의 전체 채팅 내용을 불러온다.
 	useEffect(() => {
 		console.log('socket', socket);
-		socket.on('getAllMessages', (chats: ChatDataType[]) => {
-			const data = chats.map((chat: ChatDataType) => {
-				const { id, writer, text, createdAt, updatedAt } = chat;
-				const { name, email, mobile, gitId, aboutMe, profileColor, uploadImage } = writer;
+		socket?.on('getAllMessages', (chats: any) => {
+			const data = chats.map((chat: any) => {
+				const { text, writer, room } = chat;
+				const { id, name, email, mobile, gitId, career, aboutMe, uploadImage, profileColor, createdAt, updatedAt } =
+					writer;
 
 				return {
-					id: id,
 					text: text,
-					createdAt: createdAt,
-					updatedAt: updatedAt,
+					room: room,
 					writer: {
-						id: writer.id,
+						id: id,
 						name: name,
 						email: email,
 						mobile: mobile,
 						gitId: gitId,
+						career: career,
 						aboutMe: aboutMe,
 						uploadImage: uploadImage,
 						profileColor: profileColor,
-						createdAt: writer.createdAt,
+						createdAt: createdAt,
+						updatedAt: updatedAt,
 					},
 				};
 			});
-			// console.log('서버로 보내는 data', data);
 			setChatBucket(data);
 		});
-		socket.emit('getAllMessages');
-	}, []);
+
+		// TODO: room이 바뀌면 room과 다시 연결한다.
+		socket?.emit('getAllMessages', room);
+		socket?.emit('leaveRoom', room);
+		socket?.emit('joinRoom', room);
+	}, [room]);
 
 	// TODO: 메세지를 받으면 재렌더링 한다.
 	useEffect(() => {
-		socket.on('sendMessage', ({ id, text, createdAt, updatedAt, writer }: ChatDataType) => {
-			console.log('클라에서 저장한 sendMessage', chatBucket);
-			console.log('서버에서 오는 sendMessage', { id, text, createdAt, updatedAt, writer });
-			setChatBucket([...chatBucket, { id, text, createdAt, updatedAt, writer }]);
+		socket?.on('sendMessage', ({ text, writer, project, room }: any) => {
+			// console.log('클라에서 저장한 sendMessage', chatBucket);
+			// console.log('서버에서 오는 sendMessage', { text, writer, project, room });
+			setChatBucket([...chatBucket, { text, writer, project, room }]);
 		});
 	}, [chatBucket]);
 
@@ -74,15 +77,14 @@ const WorkSpaceChat = (): JSX.Element => {
 			if (chat?.trim() === '') {
 				return;
 			}
-			// TODO: 채팅치는 순간 유저 정보를 어떻게 알고 보내지?
-			const data = {
-				id: profileInfo.id,
-				name: profileInfo.name,
-				text: chat,
-			};
 
-			console.log('채팅치는 순간의 data를 확인', data);
-			socket.emit('sendMessage', data);
+			const data = {
+				room: room,
+				name: profileInfo.name,
+				message: chat,
+			};
+			// console.log('채팅치는 순간의 data를 확인', data);
+			socket?.emit('sendMessage', data);
 			setChat('');
 		},
 		[chat],
@@ -91,8 +93,6 @@ const WorkSpaceChat = (): JSX.Element => {
 	const onChangeChatValue: OnChangeHandlerFunc = useCallback((e): void => {
 		setChat(e.target.value);
 	}, []);
-
-	console.log('여긴 Container, checkBucket', chatBucket);
 
 	return (
 		<WorkSpaceFrame listData={listData}>
