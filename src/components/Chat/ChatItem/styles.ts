463d13cdb0d9b7ccd5dcdf35a40cc2b6f1db2aa8
useonglee <useong0830@naver.com>
2021-05-27 11:49:29 +0900

    Fix #185 same sender chat item design

--- src/components/Chat/ChatItem/styles.ts
+++ src/components/Chat/ChatItem/styles.ts
@@ -1,7 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const ChatWrapper = styled.div`
-	display: flex;
+export const ChatWrapper = styled.div<{ isSameSender: boolean }>`
 	width: 100%;
 	padding: 8px 20px;
 
@@ -12,6 +11,38 @@ export const ChatWrapper = styled.div`
 	&:hover {
 		background: #eee;
 	}
+
+	${props =>
+		props.isSameSender
+			? css`
+					display: flex;
+					${({ theme }) => theme.align.flexVertical};
+			  `
+			: `
+		display: flex;
+	`}
+`;
+
+export const ChatProfileImageWrapper = styled.div<{ isSameSender: boolean }>`
+	${props =>
+		props.isSameSender
+			? css`
+					display: none;
+			  `
+			: `
+		display: block;
+	`}
+`;
+
+export const ChatUserInfoWrapper = styled.div<{ isSameSender: boolean }>`
+	${props =>
+		props.isSameSender
+			? css`
+					display: none;
+			  `
+			: `
+		display: block;
+	`}
 `;
 
 export const ChatUserId = styled.span`
@@ -26,8 +57,17 @@ export const ChatCreatedAt = styled.span`
 	color: #888888;
 `;
 
-export const ChatContent = styled.p`
+export const ChatContent = styled.p<{ isSameSender: boolean }>`
 	margin-top: 8px;
 	font-family: NanumSquareR;
 	font-size: 16px;
+
+	${props =>
+		props.isSameSender
+			? css`
+					margin: 0px 0px 0px 52px;
+			  `
+			: `
+			margin-top: 8px;
+	`}
 `;
