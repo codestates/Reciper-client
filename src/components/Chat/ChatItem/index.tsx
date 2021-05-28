211e49238e3b689c359d83fd0631ba9cc2518021
useonglee <useong0830@naver.com>
2021-05-29 04:12:39 +0900

    Move chat soket data file

--- src/components/Chat/ChatItem/index.tsx
+++ src/components/Chat/ChatItem/index.tsx
@@ -5,7 +5,7 @@ import DeleteAlert from '../DeleteAlert';
 import useSocket from '../../../hooks/useSocket';
 import useInput from '../../../hooks/useInput';
 import { getProfileInfoSelector } from '../../../reducer/profile';
-import { getChatDeleteData, getChatEditData, newChatData } from '../../../utils/sockekData';
+import { getChatDeleteData, getChatEditData, newChatData } from '../../../utils/ChatSocketData';
 
 import dayjs from 'dayjs';
 import autosize from 'autosize';
