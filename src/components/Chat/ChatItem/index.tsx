464df44de1d14deaee38e1894b71ddd69a9db0e8
useonglee <useong0830@naver.com>
2021-05-28 10:27:17 +0900

    Add #221 chat item hover feature

--- src/components/Chat/ChatItem/index.tsx
+++ src/components/Chat/ChatItem/index.tsx
@@ -1,12 +1,20 @@
-import React, { memo } from 'react';
+import React, { Dispatch, memo, SetStateAction, useCallback, useEffect, useState } from 'react';
 import ProfileImage from '../../Common/ProfileImage';
+import ChatProfileModal from '../ChatProfileModal';
+import Modal from '../../Common/Modal';
+import useSocket from '../../../hooks/useSocket';
 
 import dayjs from 'dayjs';
+import { useParams } from 'react-router';
 
 import {
 	ChatContent,
 	ChatCreatedAt,
+	ChatDeleteButton,
+	ChatEditbutton,
+	ChatNowDateHover,
 	ChatProfileImageWrapper,
+	ChatUpdateModal,
 	ChatUserId,
 	ChatUserInfoWrapper,
 	ChatWrapper,
@@ -17,16 +25,52 @@ import { ChatDataType } from '../../../types/types';
 interface Props {
 	data: ChatDataType;
 	isSameSender: boolean;
+	setChatBucket: Dispatch<SetStateAction<ChatDataType[]>>;
 }
 
-const ChatItem = ({ data, isSameSender }: Props): JSX.Element => {
+const ChatItem = ({ data, isSameSender, setChatBucket }: Props): JSX.Element => {
+	const { projectUrl, part: room } = useParams<{ projectUrl: string; part: string }>();
+	const [socket] = useSocket(projectUrl);
+
+	const [showChatProfileModal, setShowChatProfileModal] = useState<boolean>(false);
+
 	const { uploadImage, profileColor, name } = data.writer;
 	let date = dayjs(data.createdAt);
 	date = date.add(9, 'hour');
 
+	const onShowChatProfileModal = useCallback(e => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		setShowChatProfileModal(true);
+	}, []);
+
+	// console.log(data.id);
+	const onChatEditButton = useCallback(() => {
+		console.log(data.id);
+	}, []);
+
+	useEffect(() => {
+		socket?.on('deleteMessage', ({ id }) => {
+			console.log({ id });
+			// setChatBucket([...{ id }])
+		});
+	}, []);
+
+	const onChatDeleteButton = useCallback(e => {
+		console.log('room확인', room);
+		console.log(data.id);
+		e.preventDefault();
+		const getChatDeleteData = {
+			room: room,
+			id: data.id,
+		};
+		socket?.emit('deleteMessage', getChatDeleteData);
+	}, []);
+
 	return (
 		<ChatWrapper isSameSender={isSameSender}>
-			<ChatProfileImageWrapper isSameSender={isSameSender}>
+			<ChatProfileImageWrapper isSameSender={isSameSender} onClick={e => onShowChatProfileModal(e)}>
 				<ProfileImage
 					width="40px"
 					height="40px"
@@ -35,6 +79,12 @@ const ChatItem = ({ data, isSameSender }: Props): JSX.Element => {
 					userName={name}
 				/>
 			</ChatProfileImageWrapper>
+			{showChatProfileModal && (
+				<Modal backgroundColor={false} setShowModal={setShowChatProfileModal}>
+					<ChatProfileModal data={data} />
+				</Modal>
+			)}
+
 			<div>
 				<ChatUserInfoWrapper isSameSender={isSameSender}>
 					<ChatUserId>{name}</ChatUserId>
@@ -42,6 +92,11 @@ const ChatItem = ({ data, isSameSender }: Props): JSX.Element => {
 				</ChatUserInfoWrapper>
 				<ChatContent isSameSender={isSameSender}>{data.text}</ChatContent>
 			</div>
+			<ChatUpdateModal>
+				<ChatEditbutton onClick={onChatEditButton} />
+				<ChatDeleteButton onClick={onChatDeleteButton} />
+			</ChatUpdateModal>
+			<ChatNowDateHover isSameSender={isSameSender}>{dayjs(date).format('A h:mm')}</ChatNowDateHover>
 		</ChatWrapper>
 	);
 };
--- src/components/Chat/ChatItem/styles.ts
+++ src/components/Chat/ChatItem/styles.ts
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components';
+import { HiOutlineTrash, HiOutlinePencil } from 'react-icons/hi';
 
 export const ChatWrapper = styled.div<{ isSameSender: boolean }>`
+	position: relative;
 	width: 100%;
 	padding: 8px 20px;
 
@@ -10,6 +12,10 @@ export const ChatWrapper = styled.div<{ isSameSender: boolean }>`
 
 	&:hover {
 		background: #eee;
+	}
+
+	&:hover > span {
+		opacity: 1;
 	}
 
 	${props =>
@@ -23,7 +29,47 @@ export const ChatWrapper = styled.div<{ isSameSender: boolean }>`
 	`}
 `;
 
+// ------------------TODO: 채팅 수정 및 삭제 버튼 모달 ------------------
+
+export const ChatUpdateModal = styled.span`
+	opacity: 0;
+	${({ theme }) => theme.align.flexCenter};
+	flex-direction: row;
+	position: absolute;
+	top: -16px;
+	right: 40px;
+	z-index: 10;
+	width: 80px;
+	height: 30px;
+	background-color: #fff;
+	border: 1px solid ${({ theme }) => theme.color.lineColor};
+	border-radius: 30px;
+`;
+
+export const ChatEditbutton = styled(HiOutlinePencil)`
+	cursor: pointer;
+	margin: 0 2px;
+	font-size: 22px;
+	color: #555555;
+
+	&:hover {
+		color: ${({ theme }) => theme.color.pointColor};
+	}
+`;
+
+export const ChatDeleteButton = styled(HiOutlineTrash)`
+	cursor: pointer;
+	margin: 0 2px;
+	font-size: 22px;
+	color: #555555;
+
+	&:hover {
+		color: ${({ theme }) => theme.color.warningColor};
+	}
+`;
+
 export const ChatProfileImageWrapper = styled.div<{ isSameSender: boolean }>`
+	position: relative;
 	${props =>
 		props.isSameSender
 			? css`
@@ -33,6 +79,8 @@ export const ChatProfileImageWrapper = styled.div<{ isSameSender: boolean }>`
 		display: block;
 	`}
 `;
+
+// ------------------TODO: 채팅 디테일 ------------------
 
 export const ChatUserInfoWrapper = styled.div<{ isSameSender: boolean }>`
 	${props =>
@@ -70,4 +118,21 @@ export const ChatContent = styled.p<{ isSameSender: boolean }>`
 			: `
 			margin-top: 8px;
 	`}
+`;
+
+export const ChatNowDateHover = styled.span<{ isSameSender: boolean }>`
+	opacity: 0;
+	position: absolute;
+	left: 4px;
+	font-family: NanumSquareR;
+	font-size: 13px;
+	color: #6b6b6b;
+	${props =>
+		props.isSameSender
+			? css`
+					display: block;
+			  `
+			: `
+		display: none;
+`}
 `;
