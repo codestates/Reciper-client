6313e41af6f070588ea2755ae3cae54605ae977d
useonglee <useong0830@naver.com>
2021-05-30 10:23:52 +0900

    Add #244 onDragUploadImage util function

--- src/utils/imageUpload.ts
+++ src/utils/imageUpload.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, Dispatch, DragEvent } from 'react';
 import getLoginInfo from './getLoginInfo';
 
 const { accessToken, loginType } = getLoginInfo();
 
 export const changeImage = async (
 	e: ChangeEvent<HTMLInputElement>,
-	setState: React.Dispatch<React.SetStateAction<string>>,
+	setState: Dispatch<React.SetStateAction<string>>,
 ): Promise<void> => {
 	const formData = new FormData();
 
@@ -28,4 +28,35 @@ export const clickUploadImage = (currentRef: React.RefObject<HTMLInputElement>):
 	if (currentRef.current) {
 		currentRef.current.click();
 	}
+};
+
+export const onDragUploadImage = async (
+	e: DragEvent<HTMLDivElement>,
+	setDragOver: Dispatch<React.SetStateAction<boolean>>,
+	setUploadImge: Dispatch<React.SetStateAction<string>>,
+): Promise<void> => {
+	const formData = new FormData();
+
+	if (e.dataTransfer.items) {
+		for (let i = 0; i < e.dataTransfer.items.length; i++) {
+			if (e.dataTransfer.items[i].kind === 'file') {
+				const file = e.dataTransfer.items[i].getAsFile();
+				file ? formData.append('file', file) : null;
+			}
+		}
+	} else {
+		for (let i = 0; i < e.dataTransfer.files.length; i++) {
+			formData.append('file', e.dataTransfer.files[i]);
+		}
+	}
+
+	const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/postImage`, formData, {
+		headers: {
+			authorization: `Bearer ${accessToken}`,
+			loginType,
+		},
+	});
+
+	const imageData = response.data.uploadImage;
+	setUploadImge(imageData);
 };
