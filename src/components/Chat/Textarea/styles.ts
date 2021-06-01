1fd4fa8fa764378dd05f9056fbd8c47063daef65
useonglee <useong0830@naver.com>
2021-06-01 21:01:16 +0900

    Remove chat mention feature

--- src/components/Chat/Textarea/styles.ts
+++ src/components/Chat/Textarea/styles.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
-import { MentionsInput } from 'react-mentions';
 import { RiSendPlane2Fill } from 'react-icons/ri';
 import { GoMention } from 'react-icons/go';
 import { BsImages } from 'react-icons/bs';
+
+// & > ul {
+// 	overflow-y: auto;
+// 	width: 150px;
+// 	max-height: 200px;
+// 	padding: 9px 10px;
+// 	background: #fff;
+// 	border: 1px solid lightgray;
+// 	border-radius: 4px;
+// }
 
 export const ChatAreaContainer = styled.div`
 	${({ theme }) => theme.align.flexVertical};
@@ -27,6 +36,18 @@ export const ChatForm = styled.div`
 	font-size: 14px;
 `;
 
+export const ChatTextarea = styled.textarea`
+	overflow-y: auto;
+	${({ theme }) => theme.align.flexVertical};
+	position: relative;
+	resize: none;
+	line-height: 26px;
+	width: 100%;
+	height: 26px;
+	font-family: NanumSquareR;
+	font-size: 18px;
+`;
+
 // --------------------TODO: 멘션, 사진, 전송 컨테이너  --------------------
 
 export const ChatContentsWrapper = styled.div`
@@ -43,75 +64,6 @@ export const ChatContentsWrapper = styled.div`
 		transition: 0.2s;
 		background-color: ${({ theme }) => theme.color.pointColor};
 	}
-`;
-
-// --------------------TODO: 멘션 기능  --------------------
-
-export const MentionsTextarea = styled(MentionsInput)`
-	outline: none;
-	width: 100%;
-	font-family: NanumSquareR;
-	font-size: 18px;
-
-	& > div {
-		margin-top: -4px;
-	}
-
-	& > strong {
-		background: skyblue;
-	}
-
-	& > textarea {
-		outline: none;
-		resize: none !important;
-		line-height: 22px;
-		height: 44px;
-		padding: 9px 10px !important;
-		border: none;
-		border-radius: 4px;
-	}
-
-	& > ul {
-		overflow-y: auto;
-		width: 150px;
-		max-height: 200px;
-		padding: 9px 10px;
-		background: #fff;
-		border: 1px solid lightgray;
-		border-radius: 4px;
-	}
-`;
-
-export const MentionList = styled.button<{ focus: boolean }>`
-	${({ theme }) => theme.align.flexVertical};
-	flex-direction: column;
-	width: 32px;
-	padding: 12px 10px;
-	font-size: 20px;
-	color: rgb(28, 29, 28);
-	background: transparent;
-	border: none;
-	border-radius: 3px;
-
-	& > div {
-		display: flex;
-		flex-direction: row;
-		margin-bottom: 8px;
-
-		& img {
-			margin-right: 12px;
-		}
-	}
-
-	${({ focus }) =>
-		focus &&
-		`
-	display: flex;
-	align-items: center;
-	font-family: NanumSquareR;
-	color: #333333;
-	background: #f6f6f8;
-`};
 `;
 
 // --------------------TODO: 전송 버튼  --------------------
