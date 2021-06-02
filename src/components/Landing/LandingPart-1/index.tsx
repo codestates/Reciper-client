d1d6f76ac4e830567fa3dd889821cc6ed00e41c5
useonglee <useong0830@naver.com>
2021-06-03 02:07:11 +0900

    Remove remark

--- src/components/Landing/LandingPart-1/index.tsx
+++ src/components/Landing/LandingPart-1/index.tsx
@@ -21,7 +21,7 @@ const LandingFirst = (): JSX.Element => {
 
 	useEffect(() => {
 		window.addEventListener('scroll', onScroll);
-		// 컴포넌트가 언마운트 되기 직전에 이벤트를 끝낸다. 메모리 누수 방지
+		// TODO: 컴포넌트가 언마운트 되기 직전에 이벤트를 끝낸다. 메모리 누수 방지
 		return () => window.removeEventListener('scroll', onScroll);
 	}, []);
 
@@ -29,7 +29,7 @@ const LandingFirst = (): JSX.Element => {
 		// TODO: 무료체험 버튼 클릭시 모달창?
 		console.log('');
 	};
-	// 6370 부터 다시 검은색
+
 	return (
 		<>
 			<LandingFirstContainer ref={BackToTopRef}>
