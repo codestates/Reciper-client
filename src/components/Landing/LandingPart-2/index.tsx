6fa03587172bed469f3b1093b0afd0c74d4084af
useonglee <useong0830@naver.com>
2021-06-03 00:44:47 +0900

    Refactor #296 changed from parallax effect to fixed effect to image

--- src/components/Landing/LandingPart-2/index.tsx
+++ src/components/Landing/LandingPart-2/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import solo from '../../../images/solo.png';
 import Parallax from 'react-rellax-wrapper';
+import part2_1 from '../../../images/part2-1.png';
+import part2_2 from '../../../images/part2-2.png';
+import part2_3 from '../../../images/part2-3.png';
 
 import {
 	TopWrapper,
@@ -9,10 +12,23 @@ import {
 	ImageWrapper,
 	SolutionMessage,
 	SubMessage,
+	MessageWrapper,
+	MessageTitle,
+	BottomImageWrapper,
+	CopyImageWrapper,
+	SpeechBubbleWrapper,
 } from '../LandingPart-2/styles';
+import useScrollFadeIn from '../../../hooks/useScrollFadeIn';
 
 const LandingSecond = (): JSX.Element => {
 	const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+	const animatedItem = {
+		title: useScrollFadeIn({ direction: 'up', duration: 1, delay: 0.5 }),
+		part2_1: useScrollFadeIn({ direction: 'up', duration: 1, delay: 0 }),
+		part2_2: useScrollFadeIn({ direction: 'up', duration: 1, delay: 0.2 }),
+		part2_3: useScrollFadeIn({ direction: 'up', duration: 1, delay: 0.4 }),
+	};
 
 	const onScroll = () => {
 		setScrollPosition(window.pageYOffset);
@@ -34,45 +50,77 @@ const LandingSecond = (): JSX.Element => {
 						transition: `${scrollPosition > 1650 ? `0.5s` : `0`}`,
 					}}
 				>
-					<Parallax speed={5}>
-						<div style={{ opacity: `${(scrollPosition - 400) / 50}`, transition: '0.5s' }}>
+					<Parallax speed={3}>
+						<div style={{ opacity: `${(scrollPosition - 200) / 50}`, transition: '0.5s' }}>
 							토이 프로젝트를 하고 싶은데 같이 할 사람을 찾고 계신가요?
 						</div>
-						<div style={{ opacity: `${(scrollPosition - 510) / 50}`, transition: '0.5s' }}>
-							팀원들과 협업을 어디서 해야할지 막막 하시다구요?
+						<div style={{ opacity: `${(scrollPosition - 310) / 50}`, transition: '0.5s' }}>
+							팀원들을 모집했는데 협업은 어디서 부터 해야할지 고민이신가요?
 						</div>
+						<div style={{ opacity: `${(scrollPosition - 410) / 50}`, transition: '0.5s' }}>
+							넘치는 아이디어를 어디서 부터 시작해야 할지 고민이신가요?
+						</div>
+						<SolutionMessage style={{ opacity: `${(scrollPosition - 650) / 50}`, transition: '0.5s' }}>
+							이제는 레시퍼에서 한 번에 해결하세요!
+							<div style={{ opacity: `${(scrollPosition - 680) / 50}`, transition: '0.5s' }}>
+								팀원들과 힘을 모아 각자의 코드로 하나의 프로젝트를 만들어 보세요!
+							</div>
+						</SolutionMessage>
 					</Parallax>
 				</TopLeftMessage>
-
 				<ImageWrapper
 					style={{
-						display: `${scrollPosition > 2000 || scrollPosition < 100 ? `none` : `block`}`,
-						position: 'relative',
-						bottom: '700px',
-						opacity: `${scrollPosition > 1750 || scrollPosition < 740 ? `0` : `1`}`,
+						opacity: `${scrollPosition < 630 ? `0` : `1`}`,
+						display: `${scrollPosition > 1200 || scrollPosition < 150 ? `none` : `block`}`,
+						position: 'fixed',
+						top: '400px',
+						left: '1113px',
+						transition: '0.5s',
 					}}
 				>
-					<Parallax speed={-10}>
-						<img src={solo}></img>
-					</Parallax>
+					{/* scrollPosition > 1320 ||   */}
+					<img src={solo} />
 				</ImageWrapper>
 			</TopWrapper>
 
 			<BottomWrapper>
-				<div>
-					<SolutionMessage style={{ opacity: `${(scrollPosition - 1560) / 50}`, transition: '0.5s' }}>
-						이제는 레시퍼에서 한 번에 해결하세요!
-					</SolutionMessage>
-					<SubMessage>
-						<div>팀원들과 힘을 모아 각자의 코드로 하나의 레시피를 만들어 보세요!</div>
-						<br />
+				<MessageWrapper>
+					<MessageTitle {...animatedItem.title}>
+						우리들에게 프로젝트는 하나의 <span>레시피</span>와 같습니다.
+					</MessageTitle>
+					<SubMessage style={{ opacity: `${(scrollPosition - 950) / 50}`, transition: '0.5s' }}>
+						<div>결국 프로그래밍의 본질은 프로그램을 만드는 것 입니다.</div>
 						<div>
-							우리들에게 프로젝트는 하나의 <span>레시피</span>와 같습니다.
+							개발자들에게 코드는 재료와도 같습니다. 프로그램은 재료들을 모아 <br />
+							하나의 완성된 레시피를 만드는 것과 유사합니다.
 						</div>
-						<br />
-						<div>코딩 친구와 협업 스킬을 한 곳에서!</div>
+						<div>
+							토이 프로젝트는 즐겁게 프로그램을 만들자는 의미에서 생겨난 <br />
+							개발자들만의 문화입니다.
+						</div>
+						<div>
+							우리들의 목적은 다양한 사람들을 만나, 일정관리와 소통을 한 곳에서 <br />할 수 있도록 제공해주며,
+						</div>
+						<div>토이 프로젝트 문화를 발전시키는데에 있습니다.</div>
 					</SubMessage>
-				</div>
+				</MessageWrapper>
+				<BottomImageWrapper>
+					<SpeechBubbleWrapper>
+						<img src={part2_1} style={{ opacity: `${(scrollPosition - 1050) / 50}`, transition: '0.5s' }} />
+						<img src={part2_2} style={{ opacity: `${(scrollPosition - 1100) / 50}`, transition: '0.5s' }} />
+						<img src={part2_3} style={{ opacity: `${(scrollPosition - 1150) / 50}`, transition: '0.5s' }} />
+					</SpeechBubbleWrapper>
+					<CopyImageWrapper
+						style={{
+							opacity: `${scrollPosition < 630 ? `0` : `1`}`,
+							display: `${scrollPosition > 1200 || scrollPosition < 150 ? `block` : `none`}`,
+
+							transition: '0.5s',
+						}}
+					>
+						<img src={solo} />
+					</CopyImageWrapper>
+				</BottomImageWrapper>
 			</BottomWrapper>
 		</>
 	);
--- src/components/Landing/LandingPart-2/styles.ts
+++ src/components/Landing/LandingPart-2/styles.ts
@@ -4,64 +4,109 @@ export const TopWrapper = styled.div`
 	${({ theme }) => theme.align.flexCenter};
 	align-items: start;
 	width: 100%;
-	min-height: 800px;
+	height: 500px;
+	padding: 0 180px;
+	background-color: #fff;
 `;
 
 export const TopLeftMessage = styled.div`
-	${({ theme }) => theme.align.flexHorizontal};
+	display: flex;
+	justify-content: flex-start;
 	align-items: flex-end;
-	width: 50%;
-	height: 800px;
+	width: 100%;
+	height: 100%;
 	font-family: 'NanumSquareB';
-	font-size: 30px;
-	color: #777777;
+	font-size: 24px;
+	color: #555555;
 
 	& > div {
 		line-height: 52px;
 	}
 `;
 
-export const ImageWrapper = styled.div`
-	height: 800px;
-
-	& > img {
-		width: 100%;
-	}
-`;
-
-export const BottomWrapper = styled.div`
- 	${({ theme }) => theme.align.flexCenter};
-	min-height: 100vh;
-	padding-top: 72px;
-	background-color: #f8f8f8;
-	}
+export const SolutionMessage = styled.div`
+	margin-top: 32px;
+	font-family: 'NanumSquareEB';
+	font-size: 36px;
+	color: #333333;
 
 	& > div {
-		width:70%;
-		height: 400px;
-		margin-bottom: 100px;
+		margin-top: 8px;
+		font-size: 20px;
 	}
 `;
 
-export const SolutionMessage = styled.div`
-	font-family: 'NanumSquareEB';
-	font-size: 40px;
-	color: #555555;
+export const ImageWrapper = styled.div`
+	& > img {
+		object-fit: cover;
+		width: 500px;
+		height: 500px;
+	}
+`;
+
+// -----------------------TODO: 밑 쪽 -----------------------
+
+export const BottomWrapper = styled.div`
+ 	${({ theme }) => theme.align.flexHorizontal};
+	align-items: flex-start;
+	width: 100%;
+	height: 70vh;
+	padding: 100px 132px;
+	background-color: #f8f8f8;
+	}
+`;
+
+export const MessageWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	flex-direction: column;
+	width: 50%;
 `;
 
 export const SubMessage = styled.div`
-	margin-top: 60px;
-	font-family: NanumSquareB;
 	line-height: 1.2rem;
+	font-family: NanumSquareB;
 	font-size: 20px;
-	color: #888888;
+	color: #555555;
 
 	& div {
-		& span {
-			padding: 3px;
-			color: #fff;
-			background-color: ${({ theme }) => theme.color.pointColor};
-			border-radius: 4px;
-		}
+		line-height: 32px;
+		padding: 4px 0;
+	}
+`;
+
+export const MessageTitle = styled.div`
+	margin-bottom: 40px;
+	font-family: 'NanumSquareEB';
+	font-size: 28px;
+	color: #555555;
+
+	& span {
+		font-family: NanumSquareEB;
+		color: ${({ theme }) => theme.color.pointColor};
+	}
+`;
+
+export const BottomImageWrapper = styled.div``;
+
+export const SpeechBubbleWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	margin-top: -200px;
+	& > img {
+		object-fit: cover;
+		width: 252px;
+		height: 270px;
+	}
+`;
+
+export const CopyImageWrapper = styled.div`
+	margin-top: -12px;
+	text-align: center;
+	& > img {
+		object-fit: cover;
+		width: 500px;
+		height: 500px;
 	}
 `;
