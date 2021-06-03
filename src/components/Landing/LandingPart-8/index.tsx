fec0b8cbc7598bc3574b33e3846df037cc931741
useonglee <useong0830@naver.com>
2021-06-04 01:16:00 +0900

    Set #322 changed css design

--- src/components/Landing/LandingPart-8/index.tsx
+++ src/components/Landing/LandingPart-8/index.tsx
@@ -6,7 +6,6 @@ const LandingEighth = (): JSX.Element => {
 		<LandingEighthContainer>
 			<div>당신의 가치를 스스로 증명 할 차례입니다</div>
 			<div>가만히 있으면 아무것도 이루어지지 않습니다.</div>
-			<div>이론만으로는 성장할 수 없습니다.</div>
 			<div>새로운 가치를 창출할 때 성장은 이루어집니다.</div>
 		</LandingEighthContainer>
 	);
