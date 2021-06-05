dcd9231c050b00a9a8513d482c9169fc9ce3b9c6
useonglee <useong0830@naver.com>
2021-06-06 02:23:27 +0900

    Add #356 validation feature to profile edit input

--- src/components/UserProfileEdit/ProfileEditTop/index.tsx
+++ src/components/UserProfileEdit/ProfileEditTop/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { getProfileInfoSelector } from '../../../reducer/profile';
 import useInput from '../../../hooks/useInput';
 import { changeImage, clickUploadImage } from '../../../utils/imageUpload';
+import { profileAboutMeValid, profileNameValid, profileMobileValid } from '../../../utils/validations';
 
 import { useSelector } from 'react-redux';
 
@@ -17,8 +18,10 @@ import {
 	ProfileUserCard,
 	ProfileUserInfoCard,
 	ProfileImg,
+	InvalidMessage,
+	InputWrapper,
 } from '../../UserProfile/ProfileTop/styles';
-import { ProfileImageUploadButton, ProfileImageUploadWrapper, ProfileUserEmail } from './styles';
+import { EmailInfoWrapper, ProfileImageUploadButton, ProfileImageUploadWrapper, ProfileUserEmail } from './styles';
 
 const UserProfileEdit = (): JSX.Element => {
 	const profileInfo = useSelector(getProfileInfoSelector);
@@ -28,6 +31,9 @@ const UserProfileEdit = (): JSX.Element => {
 	const [mobile, onChangeMobile] = useInput<string>(profileInfo.mobile);
 	const [aboutMe, onChangeAboutMe] = useInput<string>(profileInfo.aboutMe);
 	const [stackBucket, setStackBucket] = useState<string[]>(profileInfo.stacks);
+	const [nameValidation, setNameValidation] = useState<boolean>(true);
+	const [mobileValidation, setMobileValidation] = useState<boolean>(true);
+	const [aboutMeValidation, setAboutMeValidation] = useState<boolean>(true);
 
 	// TODO: 이미지 리셋 하기
 	const onResetImage = (): void => {
@@ -38,6 +44,17 @@ const UserProfileEdit = (): JSX.Element => {
 		setImage(profileInfo.uploadImage);
 		setStackBucket(profileInfo.stacks);
 	}, [profileInfo.uploadImage]);
+
+	useEffect(() => {
+		// TODO: 유저 정보 입력 유효성 검사
+		const nameCheck = profileNameValid(name);
+		const mobileCheck = profileMobileValid(mobile);
+		const aboutMeCheck = profileAboutMeValid(aboutMe);
+
+		setNameValidation(nameCheck);
+		setMobileValidation(mobileCheck);
+		setAboutMeValidation(aboutMeCheck);
+	}, [name, mobile, aboutMe]);
 
 	return (
 		<ProfileContainer>
@@ -83,46 +100,57 @@ const UserProfileEdit = (): JSX.Element => {
 
 				<ProfileUserInfoCard>
 					<div>
-						<ProfileSubTitle>이름</ProfileSubTitle>
-						<ProfileUserInfo>
-							<Input
-								width="long"
-								height="long"
-								placeholderText="이름을 입력하세요"
-								initValue={name}
-								changeEvent={onChangeName}
-							/>
-						</ProfileUserInfo>
+						<InputWrapper>
+							<ProfileSubTitle>이름</ProfileSubTitle>
+							<ProfileUserInfo>
+								<Input
+									width="long"
+									height="long"
+									placeholderText="이름을 입력하세요"
+									initValue={name}
+									changeEvent={onChangeName}
+								/>
+							</ProfileUserInfo>
+						</InputWrapper>
+						{!nameValidation && <InvalidMessage>이름은 3글자 이상 8글자 이하여야 합니다</InvalidMessage>}
 					</div>
 					<div>
-						<ProfileSubTitle>전화번호</ProfileSubTitle>
-						<ProfileUserInfo>
-							<Input
-								width="long"
-								height="long"
-								placeholderText="ex) 010-1234-5678"
-								initValue={mobile}
-								changeEvent={onChangeMobile}
-							/>
-						</ProfileUserInfo>
+						<InputWrapper>
+							<ProfileSubTitle>전화번호</ProfileSubTitle>
+							<ProfileUserInfo>
+								<Input
+									width="long"
+									height="long"
+									placeholderText="휴대폰 번호 11자리를 입력해주세요."
+									initValue={mobile}
+									changeEvent={onChangeMobile}
+								/>
+							</ProfileUserInfo>
+						</InputWrapper>
+						{!mobileValidation && <InvalidMessage>010-xxxx-xxxx 또는 010xxxxxxxx</InvalidMessage>}
 					</div>
+					<EmailInfoWrapper>
+						<InputWrapper>
+							<ProfileSubTitle>이메일</ProfileSubTitle>
+							<ProfileUserInfo>
+								<ProfileUserEmail>{profileInfo.email}</ProfileUserEmail>
+							</ProfileUserInfo>
+						</InputWrapper>
+					</EmailInfoWrapper>
 					<div>
-						<ProfileSubTitle>이메일</ProfileSubTitle>
-						<ProfileUserInfo>
-							<ProfileUserEmail>{profileInfo.email}</ProfileUserEmail>
-						</ProfileUserInfo>
-					</div>
-					<div>
-						<ProfileSubTitle>한줄 소개</ProfileSubTitle>
-						<ProfileUserInfo>
-							<Input
-								width="long"
-								height="long"
-								placeholderText="최대 20자 내로 적어주세요"
-								initValue={aboutMe}
-								changeEvent={onChangeAboutMe}
-							/>
-						</ProfileUserInfo>
+						<InputWrapper>
+							<ProfileSubTitle>한줄 소개</ProfileSubTitle>
+							<ProfileUserInfo>
+								<Input
+									width="long"
+									height="long"
+									placeholderText="최대 15자 내로 적어주세요"
+									initValue={aboutMe}
+									changeEvent={onChangeAboutMe}
+								/>
+							</ProfileUserInfo>
+						</InputWrapper>
+						{!aboutMeValidation && <InvalidMessage>최대 15자 내로 적어주세요</InvalidMessage>}
 					</div>
 				</ProfileUserInfoCard>
 			</ProfileUserCard>
@@ -134,6 +162,9 @@ const UserProfileEdit = (): JSX.Element => {
 				image={image}
 				stackBucket={stackBucket}
 				setStackBucket={setStackBucket}
+				nameValidation={nameValidation}
+				mobileValidation={mobileValidation}
+				aboutMeValidation={aboutMeValidation}
 			/>
 		</ProfileContainer>
 	);
