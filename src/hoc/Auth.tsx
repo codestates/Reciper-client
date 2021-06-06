9f268475b6c1da7102c8473eac99605a4caeedaa
Woogie-94 <keu1106@naver.com>
2021-06-06 23:47:30 +0900

    Add #369 AuthChecker feature

---
+++ src/hoc/Auth.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+export default (Component: () => JSX.Element): (() => JSX.Element) => {
+	const AuthChecker = (): JSX.Element => {
+		const history = useHistory();
+		const loginSuccess = localStorage.getItem('loginSuccess');
+
+		if (!loginSuccess) {
+			history.push({ pathname: '/recruit', state: { isLogged: true } });
+		}
+
+		return <Component />;
+	};
+
+	return AuthChecker;
+};
