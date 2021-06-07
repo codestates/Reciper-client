2961d81ce86379475dc1a774fabb2406160ffdab
useonglee <useong0830@naver.com>
2021-06-07 23:23:15 +0900

    Add #322 css and login button

--- src/components/Landing/LandingPart-8/index.tsx
+++ src/components/Landing/LandingPart-8/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { LandingEighthContainer } from './styles';
+import React, { useEffect, useState } from 'react';
+import useScrollFadeIn from '../../../hooks/useScrollFadeIn';
+import LoginModal from '../../Common/LoginModal';
+import Modal from '../../Common/Modal';
+import { LandingEighthContainer, LoginButton, Message, MessageWrapper } from './styles';
 
 const LandingEighth = (): JSX.Element => {
+	const [scrollPosition, setScrollPosition] = useState<number>(0);
+	const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+
+	const AnimatedItem = {
+		message: useScrollFadeIn({ direction: 'late-up', duration: 1.2, delay: 0 }),
+		button: useScrollFadeIn({ direction: 'late-up', duration: 0.8, delay: 0.5 }),
+	};
+
+	console.log(scrollPosition);
+	const onScroll = () => {
+		setScrollPosition(window.pageYOffset);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', onScroll);
+		// 컴포넌트가 언마운트 되기 직전에 이벤트를 끝낸다. 메모리 누수 방지
+		return () => window.removeEventListener('scroll', onScroll);
+	}, []);
+
 	return (
 		<LandingEighthContainer>
-			<div>당신의 가치를 스스로 증명 할 차례입니다</div>
-			<div>가만히 있으면 아무것도 이루어지지 않습니다.</div>
-			<div>새로운 가치를 창출할 때 성장은 이루어집니다.</div>
+			<MessageWrapper>
+				<Message {...AnimatedItem.message}>새로운 가치를 창출할 때 성장은 이루어집니다</Message>
+				<div {...AnimatedItem.button}>
+					<LoginButton onClick={() => setShowLoginModal(true)}>바로 시작하기</LoginButton>
+				</div>
+			</MessageWrapper>
+			{showLoginModal && (
+				<Modal setShowModal={setShowLoginModal}>
+					<LoginModal />
+				</Modal>
+			)}
 		</LandingEighthContainer>
 	);
 };
--- src/components/Landing/LandingPart-8/styles.ts
+++ src/components/Landing/LandingPart-8/styles.ts
@@ -2,8 +2,39 @@ import styled from 'styled-components';
 
 export const LandingEighthContainer = styled.div`
 	width: 100%;
-	height: 100vh;
-	font-family: NanumSquareB;
+	height: 95vh;
+	font-family: NanumSquareEB;
+	background-color: #222222;
+`;
+
+export const MessageWrapper = styled.div`
+	${({ theme }) => theme.align.flexCenter};
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	font-size: 3vw;
 	color: #fff;
-	background-color: ${({ theme }) => theme.color.pointColor};
+`;
+
+export const Message = styled.div`
+	${({ theme }) => theme.align.flexCenter};
+	transition: 0.5s;
+	margin-bottom: 4vw;
+`;
+
+export const LoginButton = styled.button`
+	${({ theme }) => theme.align.flexCenter};
+	width: 15vw;
+	heigth: 30vw;
+	padding: 0.8vw;
+	font-family: NanumSquareEB;
+	font-size: 2vw;
+	color: #111;
+	background-color: #fff;
+	border-radius: 4px;
+
+	&:hover {
+		color: #000;
+		background-color: #c6c6c8;
+	}
 `;
